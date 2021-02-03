125f1581a58760662a5fae89187faa459d350bb0
Arslan Ahmad <ars.ahm6@gmail.com>
2021-02-03 14:51:42 +0500

    Implemented basic functionality

--- SQL Connectivity Test/SQL Connectivity Test/MainWindow.xaml.cs
+++ SQL Connectivity Test/SQL Connectivity Test/MainWindow.xaml.cs
@@ -24,5 +24,71 @@ namespace SQL_Connectivity_Test
         {
             InitializeComponent();
         }
+
+        private void Btn_TestConnectivity_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ValidateFields();
+                var result = ValidateConnectivity();
+                if (result)
+                {
+                    MessageBox.Show($"SQL connectivity is OK.", "Connection Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"SQL Connection could not be established.", "Connection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }    
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage(ex.Message, ex.GetType().Name);
+            }
+        }
+
+        private bool ValidateConnectivity()
+        {
+            string server = Txt_DbInstance.Text;
+            string dbname = Txt_DbName.Text;
+            bool useWindowsAuth = Chk_WindowsAuthentication.IsChecked == true;
+            string username = useWindowsAuth ? string.Empty : Txt_DbUsername.Text;
+            string password = useWindowsAuth ? string.Empty : Txt_DbPassword.Text;
+            bool connectionEstablished = false;
+
+            using (var connection = SQLConnectivityHelper.GetSQLConnection(server, dbname, useWindowsAuth, username, password))
+            {
+                connection.Open();
+                connectionEstablished = true;
+                connection.Close();
+            }
+
+            return connectionEstablished;
+        }
+
+        private void ValidateFields()
+        {
+            if (string.IsNullOrWhiteSpace(Txt_DbInstance.Text))
+            {
+                throw new Exception($"Database server name cannot be empty.");
+            }
+
+            if (Chk_WindowsAuthentication.IsChecked != true)
+            {
+                if (string.IsNullOrWhiteSpace(Txt_DbPassword.Text))
+                {
+                    throw new Exception($"Database password cannot be empty.");
+                }
+
+                if (string.IsNullOrWhiteSpace(Txt_DbUsername.Text))
+                {
+                    throw new Exception($"Database username cannot be empty.");
+                }
+            }
+        }
+
+        private void ShowErrorMessage(string message, string title = "Error")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
---
+++ SQL Connectivity Test/SQL Connectivity Test/SQLConnectivityHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Connectivity_Test
+{
+    static class SQLConnectivityHelper
+    {
+        public static SqlConnection GetSQLConnection(string server, string database, bool windowsAuth, string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(server))
+            {
+                throw new ArgumentNullException(nameof(server));
+            }
+
+            if (!windowsAuth)
+            {
+                if (string.IsNullOrWhiteSpace(username))
+                {
+                    throw new ArgumentNullException(nameof(username));
+                }
+
+                if (string.IsNullOrWhiteSpace(password))
+                {
+                    throw new ArgumentNullException(nameof(password));
+                }
+            }
+
+            var connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = server;
+            if (!string.IsNullOrWhiteSpace(database))
+            {
+                connectionStringBuilder.InitialCatalog = database;
+            }
+
+            if (windowsAuth)
+            {
+                connectionStringBuilder.IntegratedSecurity = true;
+            }
+            else
+            {
+                connectionStringBuilder.UserID = username;
+                connectionStringBuilder.Password = password;
+            }
+
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
